6d86e6db2be60a5fecbe79d6041ec69da3ab330e
leejaejeong <jaejeonglee6@gmail.com>
2025-04-28 18:45:20 +0900

    브랜드가치 ui 작업중

--- js/main.js
+++ js/main.js
@@ -4,19 +4,32 @@ $(document).ready(function () {
         let scrollTop = $(window).scrollTop();
         let offset = 0; // 약간 미리 트리거 시키는 오프셋 추가
 
-        let container01Top = $('.section_02 > .container_01').offset().top - offset;
-        let container02Top = $('.section_02 > .container_02').offset().top - offset;
-        let container03Top = $('.section_02 > .container_03').offset().top - offset;
+        let container01Top = $('.sec_02 > .container_01').offset().top - offset;
+        let container02Top = $('.sec_02 > .container_02').offset().top - offset;
+        let container03Top = $('.sec_02 > .container_03').offset().top - offset;
 
         $('.txt_box').removeClass('active');
 
         if (scrollTop >= container03Top) {
-            $('.section_02 > .container_03 > .txt_box').addClass('active');
+            $('.sec_02 > .container_03 > .txt_box').addClass('active');
         } else if (scrollTop >= container02Top) {
-            $('.section_02 > .container_02 > .txt_box').addClass('active');
+            $('.sec_02 > .container_02 > .txt_box').addClass('active');
         } else {
-            $('.section_02 > .container_01 > .txt_box').addClass('active');
+            $('.sec_02 > .container_01 > .txt_box').addClass('active');
         }
     });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.item').forEach((item, i) => {
+        ScrollTrigger.create({
+          trigger: item,
+          start: "top top",
+          end: "bottom top",
+          snap: 1 / (document.querySelectorAll('.item').length - 1),
+          scrub: 1, // 부드럽게 따라오는 느낌
+        });
+      });
+
 })
 
